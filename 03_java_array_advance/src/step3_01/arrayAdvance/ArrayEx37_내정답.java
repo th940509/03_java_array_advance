dff7a07cc166d7706adf29a3c062f399b2302fae
잉송영 <@LAPTOP-B5E5718U>
2021-04-01 12:05:46 +0900

    04.01 12:05 (집)

--- 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx37_내정답.java
+++ 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx37_내정답.java
@@ -1,5 +1,5 @@
 package step3_01.arrayAdvance;
-
+//2021/04/01 8:02
 
 /*
  *	# 2차원배열 기본문제[3단계]
@@ -20,6 +20,24 @@ public class ArrayEx37_내정답 {
 		
 		// 문제 1) 가로 합 출력
 		// 정답 1) 410, 810, 1210
+		
+		int total = 0;
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				if(i==0) {
+					total += arr[i][j];
+					garo[i] = total;
+				}
+				if(i==1) {
+					total += arr[i][j];
+					garo[i] = total;
+				}
+				if(i==2) {
+					total += arr[i][j];
+					garo[i] = total;
+				}
+			}
+		}
 		
 		// 문제 2) 세로 합 출력
 		// 정답 2) 603, 606, 609, 612
