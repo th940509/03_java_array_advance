8806f1ab915b7c027dba8e198630610104a08066
잉송영 <@LAPTOP-B5E5718U>
2021-04-05 13:17:36 +0900

    04.05 13:37 (집)

--- 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx37_내정답.java
+++ 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx37_내정답.java
@@ -1,5 +1,8 @@
 package step3_01.arrayAdvance;
-//2021/04/01 8:02
+
+import java.util.Arrays;
+
+//2021/04/04 20:35 ~ 20:42
 
 /*
  *	# 2차원배열 기본문제[3단계]
@@ -23,27 +26,44 @@ public class ArrayEx37_내정답 {
 		
 		int total = 0;
 		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
+			for(int j=0; j<4; j++) {
 				if(i==0) {
-					total += arr[i][j];
-					garo[i] = total;
+					garo[i] += arr[i][j];
 				}
 				if(i==1) {
-					total += arr[i][j];
-					garo[i] = total;
+					garo[i] += arr[i][j];
 				}
 				if(i==2) {
-					total += arr[i][j];
-					garo[i] = total;
+					garo[i] += arr[i][j];
 				}
 			}
 		}
 		
+		System.out.println(Arrays.toString(garo));
+		
 		// 문제 2) 세로 합 출력
 		// 정답 2) 603, 606, 609, 612
 
+		total = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				if(j==0) {
+					sero[j] += arr[i][j];
+				}
+				if(j==1) {
+					sero[j] += arr[i][j];
+				}
+				if(j==2) {
+					sero[j] += arr[i][j];
+				}
+				if(j==3) {
+					sero[j] += arr[i][j];
+				}
+			}
+		}
 
 
+		System.out.println(Arrays.toString(sero));
 	}
 
 }
