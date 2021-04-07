6d504990a6f62f5baaafcdf06f8e559885c01f3c
잉송영 <@LAPTOP-B5E5718U>
2021-04-07 11:00:15 +0900

    04/07 10:59 (집)

--- 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx43_내정답.java
+++ 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx43_내정답.java
@@ -1,5 +1,5 @@
 package step3_01.arrayAdvance;
-//2021/04/02 16:35 ~ 
+//2021/04/06 22:30 ~ 23:11
 import java.util.Random;
 import java.util.Scanner;
 
@@ -89,17 +89,74 @@ public class ArrayEx43_내정답 {
 		}
 		
 		//게임 시작
+		while (true) {
 		  //화면출력
 		for(int i=0; i<SIZE; i++) {
 			for(int j=0; j<SIZE; j++) {
-				
+				if(map[i][j]==PLAYER) System.out.println("옷");
+				else if(map[i][j]==WALL) System.out.println("!");
+				else if(map[i][j]==GOAL) System.out.println("G");
+				else if(map[i][j]==BALL) System.out.println("*");
+				else                     System.out.println(map[i][j]+" ");
 			}
+			System.out.println();
+		}
+		System.out.println("-------------------");
+		
+		//골대에 공을 넣으면 게임 종료
+		if (ballY == goalY && ballX == goalX) {
+			System.out.println("게임 종료");
+			break;
 		}
 		
+		System.out.print("상1하2좌3우4 입력: ");
+		int move = scan.nextInt();
+	
+		//현재 위치 저장하기
+		int yy = pY;
+		int xx = pX;
 		
+		if(move==1) yy--;
+		else if(move==2) yy++;
+		else if(move==3) xx--;
+		else if(move==4) xx++;
+		
+		//예외처리
+		if(SIZE <= xx || xx < 0) continue;
+		if(SIZE <= yy || yy < 0) continue;
+		if(map[yy][xx] == WALL || map[yy][xx] == GAOL) continue;
+		
+		//공을 만났을때
+		if (map[yy][xx] == BALL) {
+			int yyy = ballY;
+			int xxx = ballX;
+			if		(move == 1) yyy--;
+			else if (move == 2) yyy++;
+			else if (move == 3) xxx--;
+			else if (move == 4) xxx++;
+
+			// 예외처리
+			if (SIZE <= xxx || xxx < 0) continue;
+			if (SIZE <= yyy || yyy < 0) continue;
+			if (map[yyy][xxx] == WALL)  continue;
+			
+			// 공 이동시키기
+			map[ballY][ballX] = 0;
+			ballY = yyy;
+			ballX = xxx;
+			map[ballY][ballX] = BALL;
+		}
+		
+		// 캐릭터 이동시키기
+		map[pY][pX] = 0;
+		pY = yy;
+		pX = xx;
+		map[pY][pX] = PLAYER;
+		
+		
+		}
 		
 		scan.close();
-		
 	}
 
 }
